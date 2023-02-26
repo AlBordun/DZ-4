a69cd7f74b65f45b9130ae39a8eb9b1d313b22df
AleksandrB <albordunw@Gmail.com>
2023-02-26 17:51:57 +0300

    finale

--- DZ 4_1/Program.cs
+++ DZ 4_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int SumNumber(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+int SumAllDigits = SumNumber(number);
+Console.WriteLine("Сумма цифр в числе: " + SumAllDigits);
--- DZ 4_2/Program.cs
+++ DZ 4_2/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int Prompt(string message)
+{
+    System.Console.Write("Введите ряд чисел : ");
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+int[] GenArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    {
+        System.Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            System.Console.Write($"{array[i]}, ");
+        }
+        System.Console.Write($"{array[array.Length - 1]}");
+    }
+    System.Console.WriteLine("]");
+}
+int Length = Prompt("Длинна массива: ");
+int min = Prompt("Начальное значение: ");
+int max = Prompt("Конечное значение: ");
+int[] array = GenArray(Length, min, max);
+
+PrintArray(array);
